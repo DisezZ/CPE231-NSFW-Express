a239dd6642cf49c79fd50ca27518a3d214e29c78
Lutfee Deemae <lutfeedeemae@gmail.com>
2022-04-21 20:05:58 +0700

    sql tables finish datatype

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,43 +1,43 @@
 CREATE TABLE IF NOT EXISTS `Bank` (
-  `bank_id`     INT(11)     NOT NULL    AUTO_INCREMENT,
-  `manager_id`  INT(11)     NOT NULL,
-  `bank_name`   <type>      NOT NULL,
-  `created`     <type>      NOT NULL,
+  `bank_id`                     INT(11)         NOT NULL AUTO_INCREMENT,
+  `manager_id`                  INT(11)         NOT NULL,
+  `bank_name`                   VARCHAR(64)     NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`bank_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `Branch` (
-  `branch_id`       INT(11)     NOT NULL    AUTO_INCREMENT,
-  `bank_id`         INT(11)     NOT NULL,
-  `branch_name`     <type>      NOT NULL,
-  `branch_address`  <type>      NOT NULL,
-  `created`         <type>      NOT NULL,
+  `branch_id`                   INT(11)         NOT NULL AUTO_INCREMENT,
+  `bank_id`                     INT(11)         NOT NULL,
+  `branch_name`                 VARCHAR(64)     NOT NULL,
+  `branch_address`              VARCHAR(256)    NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`branch_id`),
   FOREIGN KEY (`bank_id`) REFERENCES `Bank`(`bank_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `Customer` (
-  `customer_id`     INT(11)     NOT NULL    AUTO_INCREMENT,
-  `user_id`         INT(11)     NOT NULL,
+  `customer_id`                 INT(11)         NOT NULL AUTO_INCREMENT,
+  `user_id`                     INT(11)         NOT NULL,
   PRIMARY KEY (`customer_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `Membership` (
-  `membership_id`           INT(11)     NOT NULL    AUTO_INCREMENT,
-  `membership_name`         <type>      NOT NULL,
-  `loan_amount`             <type>      NOT NULL,
-  `loan_interest_rate`      <type>      NOT NULL,
-  `deposit_interest_rate`   <type>      NOT NULL,
+  `membership_id`               INT(11)         NOT NULL AUTO_INCREMENT,
+  `membership_name`             VARCHAR(64)     NOT NULL,
+  `loan_amount`                 MONEY           NOT NULL,
+  `loan_interest_rate`          TINYINT         NOT NULL,
+  `deposit_interest_rate`       TINYINT         NOT NULL,
   PRIMARY KEY (`membership_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `Account` (
-  `account_id`      INT(11)     NOT NULL    AUTO_INCREMENT,
-  `customer_id`     INT(11)     NOT NULL,
-  `branch_id`       INT(11)     NOT NULL,
-  `membership_id`   INT(11)     NOT NULL,
-  `balance`         <type>      NOT NULL,
-  `created`         <type>      NOT NULL,
+  `account_id`                  INT(11)         NOT NULL AUTO_INCREMENT,
+  `customer_id`                 INT(11)         NOT NULL,
+  `branch_id`                   INT(11)         NOT NULL,
+  `membership_id`               INT(11)         NOT NULL,
+  `balance`                     MONEY           NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL,
   PRIMARY KEY (`account_id`),
   FOREIGN KEY (`branch_id`) REFERENCES `Branch`(`branch_id`),
   FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`customer_id`),
@@ -45,34 +45,34 @@ CREATE TABLE IF NOT EXISTS `Account` (
 );
 
 CREATE TABLE IF NOT EXISTS `ATM` (
-  `atm_id`          INT(11)     NOT NULL    AUTO_INCREMENT,
-  `branch_id`       INT(11)     NOT NULL,
+  `atm_id`                      INT(11)         NOT NULL AUTO_INCREMENT,
+  `branch_id`                   INT(11)         NOT NULL,
   PRIMARY KEY (`atm_id`),
   FOREIGN KEY (`branch_id`) REFERENCES `Branch`(`branch_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `Employee` (
-  `employee_id`     INT(11)     NOT NULL    AUTO_INCREMENT,
-  `branch_id`       INT(11)     NOT NULL,
-  `user_id`         INT(11)     NOT NULL,
+  `employee_id`                 INT(11)         NOT NULL AUTO_INCREMENT,
+  `branch_id`                   INT(11)         NOT NULL,
+  `user_id`                     INT(11)         NOT NULL,
   PRIMARY KEY (`employee_id`),
   FOREIGN KEY (`branch_id`) REFERENCES `Branch`(`branch_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `TransactionStatus` (
-  `transaction_status_id`       INT(11)     NOT NULL    AUTO_INCREMENT,
-  `transaction_status_name`     <type>      NOT NULL,
+  `transaction_status_id`       INT(11)         NOT NULL AUTO_INCREMENT,
+  `transaction_status_name`     VARCHAR(64)     NOT NULL,
   PRIMARY KEY (`transaction_status_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ATMTransaction` (
-  `atm_transaction_id`      INT(11)     NOT NULL    AUTO_INCREMENT,
-  `from_account_id`         INT(11)     NOT NULL,
-  `atm_id`                  INT(11)     NOT NULL,
-  `amount`                  <type>      NOT NULL,
-  `transaction_status_id`   INT(11)     NOT NULL,
-  `payment_approved_by`     INT(11)     NOT NULL,
-  `created`                 <type>      NOT NULL,
+  `atm_transaction_id`          INT(11)         NOT NULL AUTO_INCREMENT,
+  `from_account_id`             INT(11)         NOT NULL,
+  `atm_id`                      INT(11)         NOT NULL,
+  `amount`                      MONEY           NOT NULL,
+  `transaction_status_id`       INT(11)         NOT NULL,
+  `payment_approved_by`         INT(11)         NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`atm_transaction_id`),
   FOREIGN KEY (`from_account_id`) REFERENCES `Account`(`account_id`),
   FOREIGN KEY (`atm_id`) REFERENCES `ATM`(`atm_id`),
@@ -81,13 +81,13 @@ CREATE TABLE IF NOT EXISTS `ATMTransaction` (
 );
 
 CREATE TABLE IF NOT EXISTS `TransferTransaction` (
-  `transfer_transaction_id`     INT(11)     NOT NULL    AUTO_INCREMENT,
-  `from_account_id`             INT(11)     NOT NULL,
-  `to_account_id`               INT(11)     NOT NULL,
-  `amount`                      <type>      NOT NULL,
-  `transaction_status_id`       INT(11)     NOT NULL,
-  `payment_approved_by`         INT(11)     NOT NULL,
-  `created`                     <type>      NOT NULL,
+  `transfer_transaction_id`     INT(11)         NOT NULL AUTO_INCREMENT,
+  `from_account_id`             INT(11)         NOT NULL,
+  `to_account_id`               INT(11)         NOT NULL,
+  `amount`                      MONEY           NOT NULL,
+  `transaction_status_id`       INT(11)         NOT NULL,
+  `payment_approved_by`         INT(11)         NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`transfer_transaction_id`),
   FOREIGN KEY (`to_account_id`) REFERENCES `Account`(`account_id`),
   FOREIGN KEY (`transaction_status_id`) REFERENCES `TransactionStatus`(`transaction_status_id`),
@@ -96,15 +96,15 @@ CREATE TABLE IF NOT EXISTS `TransferTransaction` (
 );
 
 CREATE TABLE IF NOT EXISTS `Loan` (
-  `loan_id`                 INT(11)     NOT NULL    AUTO_INCREMENT,
-  `account_id`              INT(11)     NOT NULL,
-  `initial_amount`          <type>      NOT NULL,
-  `left_amount`             <type>      NOT NULL,
-  `interest_rate`           <type>      NOT NULL,
-  `transaction_status_id`   INT(11)     NOT NULL,
-  `loan_approved_by`        INT(11)     NOT NULL,
-  `payment_approved_by`     INT(11)     NOT NULL,
-  `created`                 <type>      NOT NULL,
+  `loan_id`                     INT(11)         NOT NULL AUTO_INCREMENT,
+  `account_id`                  INT(11)         NOT NULL,
+  `initial_amount`              MONEY           NOT NULL,
+  `left_amount`                 MONEY           NOT NULL,
+  `interest_rate`               TINYINT         NOT NULL,
+  `transaction_status_id`       INT(11)         NOT NULL,
+  `loan_approved_by`            INT(11)         NOT NULL,
+  `payment_approved_by`         INT(11)         NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`loan_id`),
   FOREIGN KEY (`loan_approved_by`) REFERENCES `Employee`(`employee_id`),
   FOREIGN KEY (`transaction_status_id`) REFERENCES `TransactionStatus`(`transaction_status_id`),
@@ -113,12 +113,12 @@ CREATE TABLE IF NOT EXISTS `Loan` (
 );
 
 CREATE TABLE IF NOT EXISTS `LoanPaying` (
-  `loan_paying_id`          INT(11)     NOT NULL    AUTO_INCREMENT,
-  `loan_id`                 INT(11)     NOT NULL,
-  `amount`                  <type>      NOT NULL,
-  `transaction_status_id`   INT(11)     NOT NULL,
-  `payment_approved_by`     INT(11)     NOT NULL,
-  `created`                 <type>      NOT NULL,
+  `loan_paying_id`              INT(11)         NOT NULL AUTO_INCREMENT,
+  `loan_id`                     INT(11)         NOT NULL,
+  `amount`                      MONEY           NOT NULL,
+  `transaction_status_id`       INT(11)         NOT NULL,
+  `payment_approved_by`         INT(11)         NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`loan_paying_id`),
   FOREIGN KEY (`loan_id`) REFERENCES `Loan`(`loan_id`),
   FOREIGN KEY (`transaction_status_id`) REFERENCES `TransactionStatus`(`transaction_status_id`),
@@ -126,12 +126,12 @@ CREATE TABLE IF NOT EXISTS `LoanPaying` (
 );
 
 CREATE TABLE IF NOT EXISTS `BillingTransaction` (
-  `billing_transaction_id`  INT(11)     NOT NULL    AUTO_INCREMENT,
-  `from_account_id`         INT(11)     NOT NULL,
-  `amount`                  <type>      NOT NULL,
-  `transaction_status_id`   INT(11)     NOT NULL,
-  `payment_approved_by`     INT(11)     NOT NULL,
-  `created`                 <type>      NOT NULL,
+  `billing_transaction_id`      INT(11)         NOT NULL AUTO_INCREMENT,
+  `from_account_id`             INT(11)         NOT NULL,
+  `amount`                      MONEY           NOT NULL,
+  `transaction_status_id`       INT(11)         NOT NULL,
+  `payment_approved_by`         INT(11)         NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`billing_transaction_id`),
   FOREIGN KEY (`from_account_id`) REFERENCES `Account`(`account_id`),
   FOREIGN KEY (`transaction_status_id`) REFERENCES `TransactionStatus`(`transaction_status_id`),
@@ -139,13 +139,13 @@ CREATE TABLE IF NOT EXISTS `BillingTransaction` (
 );
 
 CREATE TABLE IF NOT EXISTS `TopupTransaction` (
-  `topup_transaction_id`    INT(11)     NOT NULL    AUTO_INCREMENT,
-  `from_account_id`         INT(11)     NOT NULL,
-  `mobile_number`           <type>      NOT NULL,
-  `amount`                  <type>      NOT NULL,
-  `transaction_status_id`   INT(11)     NOT NULL,
-  `payment_approved_by`     INT(11)     NOT NULL,
-  `created`                 <type>      NOT NULL,
+  `topup_transaction_id`        INT(11)         NOT NULL AUTO_INCREMENT,
+  `from_account_id`             INT(11)         NOT NULL,
+  `mobile_number`               CHAR(10)        NOT NULL,
+  `amount`                      MONEY           NOT NULL,
+  `transaction_status_id`       INT(11)         NOT NULL,
+  `payment_approved_by`         INT(11)         NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`topup_transaction_id`),
   FOREIGN KEY (`transaction_status_id`) REFERENCES `TransactionStatus`(`transaction_status_id`),
   FOREIGN KEY (`payment_approved_by`) REFERENCES `Employee`(`employee_id`),
@@ -153,30 +153,30 @@ CREATE TABLE IF NOT EXISTS `TopupTransaction` (
 );
 
 CREATE TABLE IF NOT EXISTS `Position` (
-  `position_id`     INT(11)     NOT NULL    AUTO_INCREMENT,
-  `position_name`   <type>      NOT NULL,
+  `position_id`                 INT(11)         NOT NULL AUTO_INCREMENT,
+  `position_name`               VARCHAR(64)     NOT NULL,
   PRIMARY KEY (`position_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `User` (
-  `user_id`         INT(11)     NOT NULL    AUTO_INCREMENT,
-  `position_id`     INT(11)     NOT NULL,
-  `first_name`      <type>      NOT NULL,
-  `last_name`       <type>      NOT NULL,
-  `date_of_birth`   <type>      NOT NULL,
-  `email`           <type>      NOT NULL,
-  `mobile_number`   <type>      NOT NULL,
-  `address`         <type>      NOT NULL,
-  `sex`             <type>      NOT NULL,
-  `username`        <type>      NOT NULL,
-  `password`        <type>      NOT NULL,
-  `created`         <type>      NOT NULL,
+  `user_id`                     INT(11)         NOT NULL AUTO_INCREMENT,
+  `position_id`                 INT(11)         NOT NULL,
+  `first_name`                  VARCHAR(64)     NOT NULL,
+  `last_name`                   VARCHAR(64)     NOT NULL,
+  `date_of_birth`               DATE            NOT NULL,
+  `email`                       VARCHAR(128)    NOT NULL,
+  `mobile_number`               CHAR(10)        NOT NULL,
+  `address`                     VARCHAR(256)    NOT NULL,
+  `sex`                         CHAR(1)         NOT NULL,
+  `username`                    VARCHAR(64)     NOT NULL,
+  `password`                    VARCHAR(64)     NOT NULL,
+  `created`                     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
   FOREIGN KEY (`position_id`) REFERENCES `Position`(`position_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `Manager` (
-  `manager_id`  INT(11)     NOT NULL    AUTO_INCREMENT,
-  `user_id`     INT(11)     NOT NULL,
+  `manager_id`                  INT(11)         NOT NULL AUTO_INCREMENT,
+  `user_id`                     INT(11)         NOT NULL,
   PRIMARY KEY (`manager_id`)
 );
