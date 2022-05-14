7bdd0e30e879e7c71ae0b55915a754deaec396c2
Lutfee Deemae <lutfeedeemae@gmail.com>
2022-05-14 21:12:53 +0700

    change pool

--- routes/loginApi.js
+++ routes/loginApi.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { DATE } = require('mysql/lib/protocol/constants/types');
+const pool = require('../pool');
 const loginRouter = express.Router();
 const database = require('../pool');
 // check
@@ -10,16 +11,22 @@ loginRouter.post('/userLogin',(req, res) =>{
     if (!username || !password){
         return res.status(400).send({error: true, message: "Please insert information (/loginApi)"});
     }else{
-        database.query(`SELECT * FROM User WHERE username = ? AND password = ?;`,[username,password], (error, results) => {
-            if(error){
-                return res.send({error: error, data: results, message: 'Error occur in login mysql statemet (/login)'});
+        pool.getConnection((err, connection) => {
+            if (err) {
+                connection.release()
+                throw error
             }
-            if (results === undefined || results.length == 0){
-                return res.send({error: false, data: results, message: 'Not found username:'+ username + 'in accout table(/LoginApi)'});
-            }else{
-                return res.send({error: false, data: results, message: 'Found user:' + username + 'in accout table(/LoginApi)'});
-            }
-        })          
+            connection.query(`SELECT * FROM User WHERE username = ? AND password = ?;`,[username,password], (error, results) => {
+                if(error){
+                    return res.send({error: error, data: results, message: 'Error occur in login mysql statemet (/login)'});
+                }
+                if (results === undefined || results.length == 0){
+                    return res.send({error: false, data: results, message: 'Not found username:'+ username + 'in accout table(/LoginApi)'});
+                }else{
+                    return res.send({error: false, data: results, message: 'Found user:' + username + 'in accout table(/LoginApi)'});
+                }
+            })
+        })   
     }
 })
 
